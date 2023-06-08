8c424d2bf1926313c02927ca4dd12c4f10d550cf
Alec Musasa <alecmus@live.com>
2023-06-08 04:09:28 +0200

    Added Spring Initializr code

---
+++ src/main/java/com/github/alecmus/spring6jpadatarest/Spring6jpadatarestApplication.java
@@ -0,0 +1,13 @@
+package com.github.alecmus.spring6jpadatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6jpadatarestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6jpadatarestApplication.class, args);
+	}
+
+}
