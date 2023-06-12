0c0478b25537702600d70863b822ca9304df3d71
Alec Musasa <alecmus@live.com>
2023-06-12 11:57:00 +0200

    Customize the URL path

--- src/main/java/com/github/alecmus/spring6jpadatarest/repository/BeerRepository.java
+++ src/main/java/com/github/alecmus/spring6jpadatarest/repository/BeerRepository.java
@@ -5,10 +5,11 @@ import com.github.alecmus.spring6jpadatarest.domain.BeerStyleEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.UUID;
 
-
+@RepositoryRestResource(path = "beer", collectionResourceRel = "beer")
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
     Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
 
